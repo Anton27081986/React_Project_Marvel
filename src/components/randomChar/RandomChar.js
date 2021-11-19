b5a5bf2141ddee3709e9688f464690c4cbcada9c
Anton <anton27081986@gmail.com>
2021-11-19 11:45:39 +0300

    add charInfo & component ErrorBoundary

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,10 +8,10 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.updateChar();
+    // }
 
     state = {
         char: {},
@@ -21,10 +21,20 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
+    componentDidMount() {
+        this.updateChar();
+    }
+
     onCharLoaded = (char) => {
         this.setState({
             char, 
             loading: false
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
         })
     }
 
@@ -37,6 +47,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -67,7 +78,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -79,13 +90,17 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {thumbnail, name, description, homepage, wiki} = char;
+    let imgStyle = {'objectFit': 'cover' };
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit': 'contain'}
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
-                <p className="randomchar__descr" numberOfLines={1}>
+                <p className="randomchar__descr" numberoflines={1}>
                     {description}
                 </p>
                 <div className="randomchar__btns">
